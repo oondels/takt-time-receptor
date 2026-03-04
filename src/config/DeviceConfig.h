1bc33afb856244bc530a176d5e09eba7bab23756
Hendrius Dass <hendriusfelix.dev@gmail.com>
2026-03-04 08:11:27 -0300

    feat(config): update MQTT server address and WiFi credentials

--- src/config/DeviceConfig.h
+++ src/config/DeviceConfig.h
@@ -5,7 +5,7 @@
 constexpr const char *DEFAULT_DEVICE_ID = "cost-default-id";
 ***REMOVED***
 ***REMOVED***
-constexpr const char *DEFAULT_MQTT_SERVER = "10.57.25.135";
+***REMOVED***
 constexpr int DEFAULT_MQTT_PORT = 1883;
 constexpr int DEFAULT_TAKT_COUNT = 0;
 constexpr const char *DEFAULT_OTA_KEY = "default-ota-key";
